d289a351746ccd13aad196163d785d72026a1cf8
Fernan2 <fernandorankia@gmail.com>
2014-04-02 14:39:09 +0200

    Dropping magic numbers: let's give them a name -> cuantas_semanas_quiero_ver

--- app/controllers/kpi/semanal_controller.rb
+++ app/controllers/kpi/semanal_controller.rb
@@ -3,7 +3,8 @@
 module Kpi
   class SemanalController < ApplicationController
     def index
-      @semanas = (Date.yesterday.cweek - 4)..Date.yesterday.cweek
+      cuantas_semanas_quiero_ver = 5
+      @semanas = (Date.yesterday.cweek - cuantas_semanas_quiero_ver + 1)..Date.yesterday.cweek
       @kpi_semanales = Kpi::Semanal.where(anyo: Date.yesterday.year, semana: @semanas)
     end
   end
