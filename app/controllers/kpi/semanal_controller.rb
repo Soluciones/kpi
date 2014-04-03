c3a1169d5f20f47101906b704e9cc7a5ec2f504e
Fernan2 <fernandorankia@gmail.com>
2014-04-03 20:11:44 +0200

    The remaining semanal_controller is too small to be tested… we only want to test that all is well wired

--- app/controllers/kpi/semanal_controller.rb
+++ app/controllers/kpi/semanal_controller.rb
@@ -6,7 +6,6 @@ module Kpi
       n_semanas_que_quiero_ver = 5
       @semanas = Kpi::Semanal.dame_ultimas_n_semanas(n_semanas_que_quiero_ver)
       @kpi_semanales = Kpi::Semanal.where(anyo: Date.yesterday.year, semana: @semanas)
-      # @kpi_semanales = { semanas: dame_semanas_que_quiero_ver, datos: Kpi::Semanal.where(anyo: Date.yesterday.year, semana: dame_semanas_que_quiero_ver) }
     end
   end
 end
--- spec/controllers/kpi/semanal_controller_spec.rb
+++ spec/controllers/kpi/semanal_controller_spec.rb
@@ -5,21 +5,11 @@ require 'spec_helper'
 module Kpi
   describe SemanalController do
     describe "#index" do
+      before { FactoryGirl.create(:semanal, anyo: Time.now.year, semana: Date.today.cweek - 1) }
 
-
-      describe "debe devolver los KPI que tocan" do
-        let!(:dato_semanal_nuevo) { FactoryGirl.create(:semanal, anyo: 2014, semana: 10)}
-        let!(:dato_semanal_antiguo_pero_en_rango) { FactoryGirl.create(:semanal, anyo: 2014, semana: 6)}
-        let!(:dato_semanal_demasiado_antiguo) { FactoryGirl.create(:semanal, anyo: 2014, semana: 5)}
-        let!(:dato_semanal_del_anyo_pasado) { FactoryGirl.create(:semanal, anyo: 2013, semana: 10)}
-
-        it "devuelve los de las últimas 5 semanas" do
-          Time.stub(now: '2014-03-04 11:25'.to_datetime)
-          get :index, { use_route: :kpi }
-          ids = assigns(:kpi_semanales).map(&:id)
-          ids.length.should == 2
-          ids.should include(dato_semanal_antiguo_pero_en_rango.id, dato_semanal_nuevo.id)
-        end
+      it "debe funcionar sin errores" do
+        get :index, { use_route: :kpi }
+        response.should be_ok
       end
     end
   end
