f559e995424753c1bf2bd9fe241c2b5747230b7c
Fernan2 <fernandorankia@gmail.com>
2014-04-03 13:39:21 +0200

    Added test coverage to the Usuario's scopes

---
+++ spec/models/kpi/usuario_spec.rb
@@ -0,0 +1,50 @@
+# coding: UTF-8
+
+require 'spec_helper'
+
+describe Usuario do
+  describe "scopes" do
+    let!(:activado) { Usuario.create(nick: 'Poldo', estado_id: 1, created_at: 2.days.ago) }
+    let!(:sin_activar) { Usuario.create(nick: 'Waldo', estado_id: 0, created_at: 2.days.ago) }
+    let!(:antiguo) { Usuario.create(nick: 'Fer', estado_id: 1, created_at: 2.years.ago) }
+
+    context "en la última semana" do
+      it "filtra los activados" do
+        nombres = Usuario.activados.ultima_semana.map(&:nick)
+        nombres.should == [activado.nick]
+      end
+
+      it "filtra los sin_activar" do
+        nombres = Usuario.sin_activar.ultima_semana.map(&:nick)
+        nombres.should == [sin_activar.nick]
+      end
+
+      it "filtra todos" do
+        nombres = Usuario.todos.ultima_semana.map(&:nick)
+        nombres.length.should == 2
+        nombres.should include(activado.nick)
+        nombres.should include(sin_activar.nick)
+      end
+    end
+
+    context "totales" do
+      it "filtra los activados" do
+        nombres = Usuario.activados.map(&:nick)
+        nombres.length.should == 2
+        nombres.should include(activado.nick)
+        nombres.should include(antiguo.nick)
+      end
+
+      it "filtra los sin_activar" do
+        nombres = Usuario.sin_activar.map(&:nick)
+        nombres.should == [sin_activar.nick]
+      end
+
+      it "filtra todos" do
+        Usuario.todos.count.should == 3
+      end
+    end
+
+  end
+
+end
