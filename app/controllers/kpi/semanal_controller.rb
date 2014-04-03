39d429d4348c9c9af5b5cb3a339e18b8abeacec4
Fernan2 <fernandorankia@gmail.com>
2014-04-03 12:55:44 +0200

    extracting to explaining vars and apply our naming conventions

--- app/controllers/kpi/semanal_controller.rb
+++ app/controllers/kpi/semanal_controller.rb
@@ -3,8 +3,10 @@
 module Kpi
   class SemanalController < ApplicationController
     def index
-      cuantas_semanas_quiero_ver = 5
-      @semanas = (Date.yesterday.cweek - cuantas_semanas_quiero_ver + 1)..Date.yesterday.cweek
+      n_semanas_que_quiero_ver = 5
+      semana_mas_antigua_que_quiero_ver = Date.yesterday.cweek - n_semanas_que_quiero_ver + 1
+      semana_mas_reciente_que_quiero_ver = Date.yesterday.cweek
+      @semanas = semana_mas_antigua_que_quiero_ver..semana_mas_reciente_que_quiero_ver
       @kpi_semanales = Kpi::Semanal.where(anyo: Date.yesterday.year, semana: @semanas)
     end
   end
