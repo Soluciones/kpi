9482f69edd61bd36805180d68f7efba2b0b80808
Fernan2 <fernandorankia@gmail.com>
2014-04-03 12:35:20 +0200

    7.days are 1.week

--- app/modules/kpi/comun.rb
+++ app/modules/kpi/comun.rb
@@ -7,7 +7,7 @@ module Kpi::Comun
 
   module ComunScopes
     def scopes_comunes
-      scope :ultima_semana, -> { where(created_at: 7.days.ago.to_date..Time.now.to_date) }
+      scope :ultima_semana, -> { where(created_at: 1.week.ago.to_date..Time.now.to_date) }
     end
   end
 end
