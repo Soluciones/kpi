06da058ad8a77cac45ea822ee381c2a62ec82db3
Fernan2 <fernandorankia@gmail.com>
2014-04-04 09:59:11 +0200

    FactoryFail.delete(:quoted_fakers)

--- spec/factories/kpi/semanal.rb
+++ spec/factories/kpi/semanal.rb
@@ -4,8 +4,8 @@ FactoryGirl.define do
   factory :semanal, class: 'Kpi::Semanal' do
     anyo              { Random.rand(2010..2020) }
     semana            { Random.rand(1..53) }
-    modelo            { 'Faker::Name.first_name' }
-    scope             { 'Faker::Internet.email' }
+    modelo            { Faker::Lorem.word }
+    scope             { Faker::Lorem.word }
     dato              { Random.rand(0..500) }
   end
 end
