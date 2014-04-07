238c2fe6c7419dc2730ccb6e5505e96e3e8bbd09
Fernan2 <fernandorankia@gmail.com>
2014-04-07 11:44:54 +0200

    calcula_ultima_semana for each model & scope

--- app/models/kpi/semanal.rb
+++ app/models/kpi/semanal.rb
@@ -10,5 +10,16 @@ module Kpi
 
       semana_mas_antigua..semana_mas_reciente
     end
+
+    # Kpi::Semanal.calcula_ultima_semana
+    def self.calcula_ultima_semana
+      ultima_semana = Date.yesterday.cweek
+      self.where(anyo: Date.yesterday.year, semana: ultima_semana).delete_all
+      modelos_y_scopes = Kpi::CONFIGURACION.values.flatten
+      modelos_y_scopes.each do |modelo_y_scope|
+        dato = eval("#{ modelo_y_scope[:modelo] }.#{ modelo_y_scope[:scope] }.count")
+        self.create(anyo: Date.yesterday.year, semana: ultima_semana, modelo: modelo_y_scope[:modelo], scope: modelo_y_scope[:scope], dato: dato)
+      end
+    end
   end
 end
