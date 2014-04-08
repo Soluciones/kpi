293a1a5a06e2c6a3f1fadd98af9c421c1caa355b
Fernan2 <fernandorankia@gmail.com>
2014-04-08 10:02:22 +0200

    no blank lines should go before an `end`

--- app/modules/kpi/contenido_kpi.rb
+++ app/modules/kpi/contenido_kpi.rb
@@ -16,6 +16,5 @@ module Kpi::ContenidoKpi
     scope :autor_no_moderador, -> { where('usuario_id NOT IN (SELECT u.id FROM usuarios u WHERE u.estado_id >= ?)', Usuario::ESTADO_USUARIO_ADMIN) }
     scope :autor_experto, -> { autor_no_moderador.where("usuario_id IN (?)", Usuario.ids_de_expertos) }
     scope :autor_ni_moderador_ni_experto, -> { autor_no_moderador.where("usuario_id NOT IN (?)", Usuario.ids_de_expertos) }
-
   end
 end
