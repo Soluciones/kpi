f3e4dd9d5fe34ae09186354bb6fb7492aba244ec
Fernan2 <fernandorankia@gmail.com>
2014-04-08 09:54:04 +0200

    Cache was broking the tests, as it stored the "ids_de_expertos" of the first test and applied to all tests. I could expire it on each test, but as it's a light query, I prefer removing it

--- app/modules/kpi/usuario_kpi.rb
+++ app/modules/kpi/usuario_kpi.rb
@@ -20,11 +20,9 @@ module Kpi::UsuarioKpi
     scope :sin_activar, -> { where(estado_id: ESTADO_USUARIO_SIN_ACTIVAR) }
 
     def self.ids_de_expertos
-      Rails.cache.fetch "ids_de_expertos" do
-        ActiveRecord::Base.connection.select_values("SELECT tt.tematizable_id
-                                                       FROM tematica_tematizaciones tt
-                                                      WHERE tt.tematizable_type = 'Usuario' and tt.tematizable_grupo = '#{EXPERTO}'")
-      end
+      ActiveRecord::Base.connection.select_values("SELECT tt.tematizable_id
+                                                     FROM tematica_tematizaciones tt
+                                                    WHERE tt.tematizable_type = 'Usuario' and tt.tematizable_grupo = '#{EXPERTO}'")
     end
   end
 end
