6d7c8246e7b8d70a59271e3e250fb92acc363c6e
Fernan2 <fernandorankia@gmail.com>
2014-04-11 14:46:11 +0200

    We will use application layout inheriting our ApplicationController from main_app's

--- app/controllers/kpi/application_controller.rb
+++ app/controllers/kpi/application_controller.rb
@@ -1,4 +1,2 @@
-module Kpi
-  class ApplicationController < ActionController::Base
-  end
+class Kpi::ApplicationController < ApplicationController  # esto permite el render de las vistas usando el layout de main app
 end
