222583c9d3a76b83542d325c0138146486ef867f
David BF <burretfresket@gmail.com>
2014-12-22 14:01:20 +0100

    Use SQL BETWEEN & remove unneeded .to_date

--- app/modules/kpi/estadistica_kpi.rb
+++ app/modules/kpi/estadistica_kpi.rb
@@ -1,5 +1,3 @@
-# coding: UTF-8
-
 require 'active_support/concern'
 
 module Kpi::EstadisticaKpi
@@ -12,7 +10,7 @@ module Kpi::EstadisticaKpi
 
     belongs_to :usuario, class_name: ::Kpi::Clases.usuario_extern
 
-    scope :ultima_semana, -> { having("MIN(created_at) >= ? AND MIN(created_at) < ?", 1.week.ago.to_date, Time.now.to_date) }
+    scope :ultima_semana, -> { having('MIN(created_at) BETWEEN ? AND ?', 1.week.ago, Time.current) }
     scope :primera_participacion_del_usuario, -> { select('usuario_id, MIN(created_at) AS fecha_primera_participacion').group(:usuario_id) }
   end
 end
